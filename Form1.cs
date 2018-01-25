02c502ab864f6df880aa1c5adaa3e71bf36a741f
ShadrikovaPolina <35374835+ShadrikovaPolina@users.noreply.github.com>
2018-01-25 13:33:29 +0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba2
+{
+    public partial class Form1 : Form
+    {
+        Color color;
+        Color dopColor;
+        int maxSpeed;
+        int mass;
+
+        private Ianimal animal;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            color = Color.LightGreen;
+            dopColor = Color.Brown;
+            maxSpeed = 150;
+            mass = 200;
+            textBox1.Text = "" + maxSpeed;
+            textBox2.Text = "" + mass;
+        }
+
+        private bool checkFields()
+        {
+            if (!int.TryParse(textBox1.Text, out maxSpeed))
+            {
+                return false;
+            }
+            if (!int.TryParse(textBox2.Text, out mass))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (checkFields())
+            {
+                animal = new crocodile(150, color);
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                animal.draw_croc(gr);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (checkFields())
+            {
+                animal = new alligator(true, 150, color, dopColor);
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                animal.draw_croc(gr);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (animal != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                animal.move_croc(gr);
+                pictureBox1.Image = bmp;
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                color = cd.Color;
+                button4.BackColor = color;
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                dopColor = cd.Color;
+                button5.BackColor = dopColor;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ crocodile.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2
+{
+    class crocodile : Chordata
+    {
+        public override int maxSpeed
+        {
+            get
+            {
+                return base.maxSpeed;
+            }
+            protected set
+            {
+                if (value > 100 && value < 300)
+                {
+                    base.maxSpeed = value;
+                }
+                else
+                {
+                    base.maxSpeed = 150;
+                }
+            }
+        }
+
+        public crocodile(int maxSpeed, Color color)
+        {
+            this.maxSpeed = maxSpeed;
+            this.ColorBody = color;
+            Random rand = new Random();
+            startPosx = rand.Next(100, 200);
+            startPosy = rand.Next(100, 200);
+        }
+
+        public override void move_croc(Graphics g)
+        {
+            startPosx += maxSpeed;
+            draw_croc(g);
+        }
+        public override void draw_croc(Graphics g)
+        {
+            draw_sencroc(g);
+        }
+        protected virtual void draw_sencroc(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+            PointF[] points = new PointF[10];
+            points[0] = new PointF(startPosx, startPosy);
+            points[1] = new PointF(startPosx, startPosy - 2);
+            points[2] = new PointF(startPosx - 18, startPosy - 8);
+            points[3] = new PointF(startPosx - 27, startPosy - 6);
+            points[4] = new PointF(startPosx - 58, startPosy - 7);
+            points[5] = new PointF(startPosx - 90, startPosy);
+            points[6] = new PointF(startPosx - 58, startPosy + 7);
+            points[7] = new PointF(startPosx - 27, startPosy + 6);
+            points[8] = new PointF(startPosx - 18, startPosy + 8);
+            points[9] = new PointF(startPosx, startPosy + 2);
+            Brush br = new SolidBrush(ColorBody);
+            g.FillPolygon(br, points);
+            g.FillEllipse(br, startPosx - 60, startPosy - 11, 40, 22);
+
+            g.FillEllipse(br, startPosx - 32, startPosy - 20, 5, 15);
+            g.FillEllipse(br, startPosx - 52, startPosy - 20, 5, 15);
+            g.FillEllipse(br, startPosx - 52, startPosy + 5, 5, 15);
+            g.FillEllipse(br, startPosx - 32, startPosy + 5, 5, 15);
+
+            Brush br1 = new SolidBrush(Color.Yellow);
+            g.FillEllipse(br1, startPosx - 10, startPosy - 7, 5, 5);
+            g.FillEllipse(br1, startPosx - 10, startPosy + 2, 5, 5);
+        }
+    }
+}
